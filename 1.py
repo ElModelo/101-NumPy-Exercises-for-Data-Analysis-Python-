0f9cf99a8b59d9905f4caaecaeb34a22f1f2d868
Fernando Cornielle <Fernandocornielle@gmail.com>
2019-02-15 16:26:26 -0400

    firsts problems of 101 NumPy Exercises for Data Analysis (Python)

---
+++ 1.py
@@ -0,0 +1,3 @@
+# 1. Import numpy as np and see the version
+import numpy as np
+print(np.__version__)
---
+++ 2.py
@@ -0,0 +1,4 @@
+# 2. How to create a 1D array?
+import numpy as np
+a = np.arange(0,10,1)
+print(a)
